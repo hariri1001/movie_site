d8fc323e26c9c17f483f6c14fa300f57f038e2dd
이하리 <asdzx1320@example.com>
2024-11-26 06:13:42 +0900

    1126

--- vue-project/src/api/index.js
+++ vue-project/src/api/index.js
@@ -1,28 +1,3 @@
-// import axios from 'axios'
-
-// const api = axios.create({
-//   baseURL: 'http://127.0.0.1:8000',
-//   headers: {
-//     'Authorization': `Token ${JSON.parse(localStorage.getItem('counter')).token}`
-//   }
-// })
-
-// export default api
-// import axios from 'axios'
-
-// const getToken = () => {
-//   const store = localStorage.getItem('counter')
-//   return store ? JSON.parse(store).token : null
-// }
-
-// const api = axios.create({
-//   baseURL: 'http://127.0.0.1:8000',
-//   headers: {
-//     'Authorization': `Token ${getToken() || ''}`
-//   }
-// })
-
-// export default api
 
 
 
